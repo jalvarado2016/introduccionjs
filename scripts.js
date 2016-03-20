002e6ef4e5bed4af23b08e14f742da151c8a50fe
luisjacome <luis1987_m@live.com>
2016-03-20 15:47:38 -0500

    errores de lineas corregidos

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 console.log("Inicio de programa");
 alert("hola JavaScript");
-alert("Tratando de aprender JS)
-document.write("<h1>Hola JavaScript!</h1>");
-document.writ("<h2>Hola JavaScript!</h2";
-alert("Gracias por revisar")	
+alert("Tratando de aprender JS");
+document.write("<h1>Hola JavaScript!</p>");
+document.write("<h2>Hola JavaScript!</p>");
+alert("Gracias por revisar");	
 alert("Gracias por pasar");
 console.log("Fin de Programa");
