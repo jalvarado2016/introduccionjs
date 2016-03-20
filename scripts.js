5f5c715c7dc472507005980a2dea0375e7143fc2
Elena <elyborme@gmail.com>
2016-03-20 17:06:59 -0500

    Lee la cantidad de caracteres y los proyecta en consola

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
-console.log("Inicio de programa");
-alert("hola JavaScript");
-alert("Tratando de aprender JS");
-document.write("<h1>Hola JavaScript!</p>");
-document.write("<h2>Hola JavaScript!</p>");
-alert("Gracias por revisar");	
-alert("Gracias por pasar");
+/***console.log("Inicio de Programa");
+var equipo =prompt("Cual es tu equipo de futbol favorito?");
+document.write("<h1>Tu equipo favorito es: </h1>"+equipo);
 console.log("Fin de Programa");
+
+var mensaje =("Hola como estas?");
+mensaje += "Nos alegra que visites la pagina de nuestra organizacion";***/
+
+var clave =prompt("Hola Usuario, Ingrese su clave en minusculas!");
+console.log("La longitud de la clave es:" + clave.length);
+console.log("TA-DA!!! Su clave es: " + clave.toUpperCase());
+console.log("El valor original de la clave es:" + clave);
