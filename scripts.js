b3a797f365576721a17433b5db35ad0e039c07e7
Elena <elyborme@gmail.com>
2016-03-20 18:13:47 -0500

    Juego del cuenta cuento

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
-/***console.log("Inicio de Programa");
-var equipo =prompt("Cual es tu equipo de futbol favorito?");
-document.write("<h1>Tu equipo favorito es: </h1>"+equipo);
+console.log("Inicio de Programa");
+var sustantivo =prompt("Ingrese un sustantivo?");
+var adjetivo =prompt("Ingrese un adjetivo?");
+var verbo =prompt("Ingrese un verbo?");
+document.write("<h1>Te cuento un cuento: </h1>");
+document.write("<p>Se dice que el " +sustantivo+ " " +adjetivo+ " le gusta " +verbo+ " en la casa del vecino.</p>");
+document.write("<p>El " +verbo+  " del " +sustantivo+ " asustaba por su " +adjetivo+ " feroz.</p>");
+document.write("<p>Al verlo que era " +adjetivo+ " todos salieron despavoridos por el " +verbo+ " del tremendisimo " +sustantivo+ " .</p>");
 console.log("Fin de Programa");
 
-var mensaje =("Hola como estas?");
-mensaje += "Nos alegra que visites la pagina de nuestra organizacion";***/
 
-var clave =prompt("Hola Usuario, Ingrese su clave en minusculas!");
-console.log("La longitud de la clave es:" + clave.length);
-console.log("TA-DA!!! Su clave es: " + clave.toUpperCase());
-console.log("El valor original de la clave es:" + clave);
