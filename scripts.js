2ae5974fd2e143d5b288414dd64053106d7a8493
John <jois.alvanez@gmail.com>
2016-03-20 15:16:43 -0500

    Archivo con errores JS

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 console.log("Inicio de programa");
 alert("hola JavaScript");
+alert("Tratando de aprender JS)
 document.write("<h1>Hola JavaScript!</h1>");
+document.writ("<h2>Hola JavaScript!</h2";
+alert("Gracias por revisar")	
 alert("Gracias por pasar");
 console.log("Fin de Programa");
